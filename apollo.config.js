e10db2e4a29c43244235a00ea93305000f550226
Prizmahosting <mosyan.gurgen.hosting@mail.ru>
2024-03-27 13:06:12 +0400

    first commit

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	service: {
 		endpoint: {
-			url: 'http://localhost:4200/api/mygraphql',
+			url: 'https://back.скоропраздник.рф/api/mygraphql',
 			skipSSLValidation: true,
 		},
 	},
