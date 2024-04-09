8aac759d90119e37d9e4b771ffc5c9b9ad55327b
Prizmahosting <mosyan.gurgen.hosting@mail.ru>
2024-04-10 02:15:15 +0400

    first commit

--- src/components/screens/product/about/about/ProductSingleAboutDescription.tsx
+++ src/components/screens/product/about/about/ProductSingleAboutDescription.tsx
@@ -5,7 +5,7 @@ import styles from './ProductSingleAboutDescription.module.scss'
 const ProductSingleAboutDescription: FC<ICurrentProduct> = ({ product }) => {
 	return (
 		<div className={styles.description}>
-			<h2 className={styles.title}>Характеристики</h2>
+			<h2 className={styles.title}>Описание</h2>
 			<div
 				className={styles.content}
 				dangerouslySetInnerHTML={{ __html: product.description }}
