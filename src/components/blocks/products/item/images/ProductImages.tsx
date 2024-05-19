b4bf16e0691ce4b929fce47f782719494aa25234
Prizmahosting <mosyan.gurgen.hosting@mail.ru>
2024-05-19 17:25:32 +0400

    first commit

--- src/components/blocks/products/item/images/ProductImages.tsx
+++ src/components/blocks/products/item/images/ProductImages.tsx
@@ -1,53 +1,27 @@
 'use client'
 
-import FilledImage from '@/components/ui/common/image/FilledImage'
+import StaticImage from '@/components/ui/common/image/StaticImage'
 import { PUBLIC_PAGES } from '@/constants/url.constants'
-import { useSwiper } from '@/hooks/helpers/slider/useSwiper'
 import type { TypeColor } from '@/shared/types/color/color.type'
 import Link from 'next/link'
 import type { FC } from 'react'
 import 'swiper/css'
-import { Pagination } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
 import styles from '../../Products.module.scss'
 import type { IProductProps } from '../../interface/products.interface'
 
 const ProductImages: FC<IProductProps & TypeColor> = ({ product, color }) => {
-	const { setSwiper, setBeginning, setEnd } = useSwiper()
 	const images = color?.images || product.images
 
 	return (
 		<div className={styles.slider}>
-			<Swiper
-				className={styles.images}
-				modules={[Pagination]}
-				spaceBetween={0}
-				slidesPerView={1}
-				slidesPerGroup={1}
-				grabCursor
-				pagination={{
-					clickable: true,
-					bulletClass: styles.bullet,
-					bulletActiveClass: styles.bulletActive,
-					el: '#imagesBullets',
-					type: 'bullets',
-				}}
-				speed={400}
-				onSwiper={(swiper) => setSwiper(swiper)}
-				onSlideChange={({ isBeginning, isEnd }) => {
-					setBeginning(isBeginning)
-					setEnd(isEnd)
-				}}
-			>
-				{images.map((image, index) => (
-					<SwiperSlide key={index} className={styles.image}>
-						<Link href={PUBLIC_PAGES.PRODUCT(product.slug)}>
-							<FilledImage src={image} alt={product.name} />
-						</Link>
-					</SwiperSlide>
-				))}
-				<div id="imagesBullets" className={styles.dots}></div>
-			</Swiper>
+			<Link href={PUBLIC_PAGES.PRODUCT(product.slug)}>
+				<StaticImage
+					src={images[0]}
+					width={200}
+					height={200}
+					alt={product.name}
+				/>
+			</Link>
 		</div>
 	)
 }
